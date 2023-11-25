256fe9af25c6c1ba52d81bb4c57e24269f4ad1da
alejoevilches <alejoevilches@gmail.com>
2023-11-25 12:26:46 -0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import "./Navbar.css";
+
+export function Navbar(){
+    return (
+        <header>
+            <img className="logo" src="/img/logo.svg" alt="River ID logo" />
+        </header>
+    )
+}
