a6fefdb9a486dc6c968a6bb5c4045e5b31ea8767
alejoevilches <alejoevilches@gmail.com>
2024-01-23 17:48:33 -0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import "./Navbar.css"
+
+export function Navbar () {
+  return (
+    <nav>
+      <img className="nav_logo" src='https://riveridstorage.blob.core.windows.net/appcarpblob/RiverID_Nuevo.svg' alt='River ID Logo' style={{ width: '180px' }} />
+    </nav>
+  )
+}
