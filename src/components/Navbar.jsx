587dd9682a3da52fe29131a0dd16314239ccffe1
alejoevilches <alejoevilches@gmail.com>
2024-01-23 17:54:27 -0300

    add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import "./Navbar.css"
+import './Navbar.css'
+import { IconMenu2 } from '@tabler/icons-react'
 
 export function Navbar () {
   return (
     <nav>
-      <img className="nav_logo" src='https://riveridstorage.blob.core.windows.net/appcarpblob/RiverID_Nuevo.svg' alt='River ID Logo' style={{ width: '180px' }} />
+      <img className='nav_logo' src='https://riveridstorage.blob.core.windows.net/appcarpblob/RiverID_Nuevo.svg' alt='River ID Logo' style={{ width: '180px' }} />
+      <IconMenu2 color="white" size={36} />
     </nav>
   )
 }
